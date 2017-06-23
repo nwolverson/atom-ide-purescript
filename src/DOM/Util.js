360ebebaf633fcd0d40813fde92e5587d12d6f08
Jens Krause <email@jkrause.io>
2017-06-23 10:56:52 +0200

    Remove JS functions
    - `getScrollTop`
    - `setScrollTop`
    - `getScrollHeight`

--- src/DOM/Util.js
+++ src/DOM/Util.js
@@ -1,25 +1,5 @@
 // module DOM.Util
 
-exports.getScrollTop = function(e) {
-  return function() {
-    return e.scrollTop;
-  };
-};
-
-exports.setScrollTop = function(e) {
-  return function(value) {
-    return function() {
-      e.scrollTop = value;
-    };
-  };
-};
-
-exports.getScrollHeight = function (e) {
-  return function() {
-    return e.scrollHeight;
-  };
-};
-
 exports.setTimeout = function(n) {
   return function (f) {
     return function() {
